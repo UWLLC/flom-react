35eb5bab1552701a7187bea363b801f1c7cf36a1
mcmillanmajora <mcmillan.majora@gmail.com>
2025-03-14 11:29:15 -0700

    ParagraphRender newlines
    Fixed char used for newline split from '/n' to '\n'

--- client/src/components/ParagraphRender/index.jsx
+++ client/src/components/ParagraphRender/index.jsx
@@ -1,7 +1,7 @@
 function ParagraphRender({ intro, id }) {
   return (
     <>
-      {intro.split('/n').map((item, idx) => (
+      {intro.split('\n').map((item, idx) => (
         <p key={id + idx}>{item}</p>
       ))}
     </>
